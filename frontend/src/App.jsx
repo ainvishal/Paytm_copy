4255bab22eb602ffe664ac1a16cd91627bb55972
ainvis <109056582+ainvishal@users.noreply.github.com>
2024-02-11 12:44:32 +0530

    further changes added 5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup/>}/>
-        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/" element={<Signin/>}/>
         <Route path="/dashbord" element={<Dashbord/>}/>
         <Route path="/send" element={<Sendmoney/>}/>
       </Routes>
