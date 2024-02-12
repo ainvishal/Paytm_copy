87c89487d748f944c73bbd2ccd7044d5524cd949
ainvis <109056582+ainvishal@users.noreply.github.com>
2024-02-12 18:52:55 +0530

    further changes added 9

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const Rootrouter = require('./routes/index')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express();
 const port = 3000;
 
 
-app.use(cors({
-    origin:'https://paytm-copy-client.vercel.app'
-}))
+// app.use(cors({
+//     origin:'https://paytm-copy-client.vercel.app'
+// }))
 
 app.use((res, req, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://paytm-copy-client.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
