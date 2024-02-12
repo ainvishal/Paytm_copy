5da7f27702c6407ea923bde42b357c2287e0eefe
ainvis <109056582+ainvishal@users.noreply.github.com>
2024-02-12 18:59:40 +0530

    further changes added 9

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const Rootrouter = require('./routes/index')
-// const cors = require('cors')
+const cors = require('cors')
 const app = express();
 const port = 3000;
 
+app.use(cors({
+    origin: 'https://paytm-copy-client.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+  }));
 
-// app.use(cors({
-//     origin:'https://paytm-copy-client.vercel.app'
-// }))
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://paytm-copy-client.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "https://paytm-copy-client.vercel.app");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// })
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1', Rootrouter)
