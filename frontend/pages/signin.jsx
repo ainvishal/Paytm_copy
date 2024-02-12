f90744053b05ee608ee677fde590e7aeb7159514
ainvis <109056582+ainvishal@users.noreply.github.com>
2024-02-12 19:06:15 +0530

    further changes added 9

--- frontend/pages/signin.jsx
+++ frontend/pages/signin.jsx
@@ -29,7 +29,7 @@ export function Signin() {
                                 },{
                                     headers: {
                                       'Content-Type': 'application/json',
-                                      'Access-Control-Allow-Origin': '*',
+                                      'Access-Control-Allow-Origin': 'https://paytm-copy-client.vercel.app',
                                       'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
                                       'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE, OPTIONS'
                                     }
